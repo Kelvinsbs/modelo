001123fb4d124d356c1d2dc6acdbdc2ab4b53aa8
Kelvin Barcelos <nivlek.barcelos@gmail.com>
2022-09-09 23:27:10 -0300

    segundo commit

--- src/atividade.js
+++ src/atividade.js
@@ -18,18 +18,19 @@ function App() {
     <div className="App">
       <div className="App-header">
         <div>
-          <input ref={inputEl} id="descricaoProjeto" type="text" placeholder="Descricao do projeto" value={value} onChange={event => setValue(event.target.value)} />
+          <input ref={inputEl} id="descricaoAtividade" type="text" placeholder="Descricao da atividade" value={value} onChange={event => setValue(event.target.value)} />
+          <input ref={inputEl} id="dataCadastro" type="date" value={value} onChange={event => setValue(event.target.value)} />
+          
+          <select>
+            <option value="1">teste1</option>
+            <option value="2">teste2</option>
+            <option value="3">teste3</option>
+            <option value="4">teste4</option>
+          </select>
+
           <button onClick={handleClick}>Adicionar</button>
         </div>
         <div>
-          <ul>
-            {
-              data.map((item, index) => (
-                <TodoItem name={item} key={index}>
-                </TodoItem>
-              ))
-            }
-          </ul>
         </div>
         
       </div>
